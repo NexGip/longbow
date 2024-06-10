c484778a1cddd3b5c7332e97fd7d3844f958f730
byscript <ozedsnow@gmail.com>
2024-06-10 22:35:02 +0100

    koss

--- app/components/js/sendMail.ts
+++ app/components/js/sendMail.ts
@@ -1,8 +1,5 @@
-import {
-  Configuration,
-  EmailsApi,
-  EmailMessageData,
-} from "@elasticemail/elasticemail-client-ts-axios";
+const zeptomail = require("zeptomail");
+const { SendMailClient } = zeptomail;
 import { MailProp } from "./dataTypes";
 import { COMPANYNAME } from "./config";
 
@@ -10,28 +7,26 @@ export const sendMassMail = async (
   details: MailProp,
   sender: { name: string; email: string } = {
     name: "Longbow Investment",
-    email: "longbow@mifxfinance.com",
+    email: "support@longbowltd.com",
   }
 ) => {
   const date = new Date().getFullYear();
-const logo=`https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.com/o/Longbow%2Fcorporate%2Flogo.png?alt=media&token=f3bd8440-a316-45ba-9dbf-1666d8b31210`
-const image=`https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.com/o/Longbow%2Fcorporate%2Fcrypto.webp?alt=media&token=6a28cf39-62e6-4ea4-8da3-58eac2b2942e`
+  const logo = `https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.com/o/Longbow%2Fcorporate%2Flogo.png?alt=media&token=f3bd8440-a316-45ba-9dbf-1666d8b31210`;
+  const image = `https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.com/o/Longbow%2Fcorporate%2Fcrypto.webp?alt=media&token=6a28cf39-62e6-4ea4-8da3-58eac2b2942e`;
 
   try {
-    const { emails:emailz, showImage } = details;
+    const { emails: emailz, showImage } = details;
 
-   const emails=emailz.length<=150 ? emailz: emailz.slice(0,150)
+    const emails = emailz.length <= 150 ? emailz : emailz.slice(0, 150);
+    const url = "api.zeptomail.com/";
+    const token =
+      "Zoho-enczapikey wSsVR61x/h/4D/h0lGGqc+dqm1tcAQ6iQUos2Vuo7CD8G6zA9Mc9nxXPV1f0G6IWF2A4HTtA9rwvzktShjoKj44kmVpWXiiF9mqRe1U4J3x17qnvhDzKVmRYlBaNLYsOzw5jn2loFctu";
 
-    const config = new Configuration({
-      apiKey:
-        "BA837FEBECE4E8C329C12EE80A813A9A91C4473A4E9B690631B42A90C70F9A5EA856AB770C6471FB5EDA1E87F818335E",
-    });
-
-    const emailsApi = new EmailsApi(config);
+    const client = new SendMailClient({ url, token });
 
     if (emails.length < 5) {
       for (let i = 0; i < emails.length; i++) {
-        const mail=emails[i];
+        const mail = emails[i];
         const message = `<div style="background-color:#d1d1d1;color:#000012; padding: 0px 5%;font-family: Open Sans, sans-serif ">
 
         <div style="width: 100%; box-sizing: border-box;text-align:center;   padding:12px;">
@@ -71,29 +66,23 @@ const image=`https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.co
             }</p>
       <p>&copy; ${date} <a style="text-decoration:none; color:inherit;">longbowltd.com</a> All Rights Reserved</p>
           </div> </div>`;
-        const emailMessageData: EmailMessageData = {
-          Recipients: [
+
+        await client.sendMail({
+          from: {
+            address: sender.email,
+            username: sender.name,
+          },
+          to: [
             {
-              Email: emails[i].email.email,
-              Fields: {
-                name: emails[i].email.name,
+              email_address: {
+                address: mail.email.email,
+                username: mail.email.name,
               },
             },
           ],
-          Content: {
-            Body: [
-              {
-                ContentType: "HTML",
-                Charset: "utf-8",
-                Content: message,
-              },
-            ],
-            From: `${sender.name} <${sender.email}>`,
-            Subject: emails[i].subject,
-          },
-        };
-
-        await emailsApi.emailsPost(emailMessageData);
+          subject: mail.subject,
+          htmlbody: message,
+        });
       }
     } else {
       const message = `<div style="background-color:#d1d1d1;color:#000012; padding: 0px 5%;font-family: Open Sans, sans-serif ">
@@ -133,30 +122,20 @@ const image=`https://firebasestorage.googleapis.com/v0/b/storer-268bd.appspot.co
           }</p>
     <p>&copy; ${date} <a style="text-decoration:none; color:inherit;">longbowltd.com</a> All Rights Reserved</p>
         </div> </div>`;
-      const emailMessageData: EmailMessageData = {
-        Recipients: emails.map((e) => {
-          const detail = {
-            Email: e.email.email,
-            Fields: {
-              name: e.email.name,
-            },
-          };
-          return detail;
-        }),
-        Content: {
-          Body: [
-            {
-              ContentType: "HTML",
-              Charset: "utf-8",
-              Content: message,
-            },
-          ],
-          From: `${sender.name} <${sender.email}>`,
-          Subject: emails[0].subject,
+      await client.sendMail({
+        from: {
+          address: sender.email,
+          username: sender.name,
         },
-      };
-
-      await emailsApi.emailsPost(emailMessageData);
+        to: emails.map((e) => ({
+          email_address: {
+            address: e.email.email,
+            username: e.email.name,
+          },
+        })),
+        subject: emails[0].subject,
+        htmlbody: message,
+      });
     }
     return true;
   } catch (error) {
